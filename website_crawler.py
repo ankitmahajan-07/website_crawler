6fb40de5f41f6caea7fc0cda6835975b60caaff5
ankitmahajan-07 <ankitmahajan478@gmail.com>
2019-12-25 15:30:42 +0530

    adding functionality for downloading css and js files

--- website_crawler.py
+++ website_crawler.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import urllib.request
 import os
+import cssutils
 url = ''
 def createProjectDirectory():
     path = input('Enter Project Name : ')
@@ -20,6 +21,8 @@ def getIndexHtml(indexFile, path):
     r = requests.get(url)
     content = BeautifulSoup(r.content, "html5lib")
     indexFile.write(str(content))
+    findCss(content, path)
+    findJs(content,path)
     downloadImages(content, path)
     findAllAnchors(content, path)
 
@@ -46,20 +49,37 @@ def storeHrefs(allAnchors, path):
 
 def makingFilesFromHrefs(hrefs, path):
     for i in hrefs:
-        if i == '/' or '/' not in i or i[0] == '#':
+        print('working on '+i)
+        if i == '/' or i[0] == '#':
             continue
+        if '/' not in i and i[0:4] != 'http' and i != 'index.html':
+            if i[-4:] == 'html':
+                newUrl = url +'/'+ i
+                print('creating file',i)
+                file = makeHtmlFile(i, path, newUrl)
+                continue
+            else:
+                fileName = i[1:] + '.html'
+                print('creating file',fileName)
+                newUrl = url +'/'+ i
+                file = makeHtmlFile(fileName, path, newUrl)
+                continue
         if i[0:5] == 'https' or i[0:4] == 'http':
             continue
         count = countSlashes(i)
         if count == 1:
             fileName = i[1:] + '.html'
             newUrl = url + i
+            print('creating file', fileName)
             file = makeHtmlFile(fileName, path,newUrl)
+            continue
 
         if count == 2 and i[-1] == '/':
             fileName = i[1:-1] + '.html'
             newUrl = url + i[:-1]
+            print('creating file', fileName)
             file = makeHtmlFile(fileName, path,newUrl)
+            continue
         else :
             makeFoldersandFiles(i, path)
 
@@ -75,7 +95,8 @@ def makeFoldersandFiles(i, path):
     if i[-1] == '/':
         i = i[0:-1]
     sortStr = i.split('/')
-    sortStr[-1] = sortStr[-1] + '.html'
+    if sortStr[-1][-4:] != 'html':
+        sortStr[-1] = sortStr[-1] + '.html'
     for j in sortStr:
         if '.' not in j:
             path = path + '\\'+j
@@ -84,6 +105,7 @@ def makeFoldersandFiles(i, path):
         if j == sortStr[-1]:
             createSubDirectories(path)
             newUrl = url+ '/'+sortStr[-1]
+            print('creating file',j)
             makeHtmlFile(j, path,newUrl)
 
 def makeHtmlFile(fileName, path,url):
@@ -93,6 +115,8 @@ def makeHtmlFile(fileName, path,url):
         content = BeautifulSoup(r.content, "html5lib")
         file.write(str(content))
         downloadImages(content, path)
+        findCss(content,path)
+        findJs(content,path)
         return file
     except OSError:
         print("Creation of the directory %s failed" % path)
@@ -104,6 +128,7 @@ def createSubDirectories(path):
         print("Creation of the sub-directory %s failed" % path)
 
 def downloadImages(content, path):
+    print('Downloading images')
     global url
     imgs = content.find_all("img")
     srcs = []
@@ -111,18 +136,82 @@ def downloadImages(content, path):
         raw_src = str(img).split('src="')
         src = raw_src[1].split('"')
         srcs.append(src[0])
+    print(srcs)
     for src in srcs:
-        if src[0:3] == 'http':
-            fileName = path+'/'+src
-            urllib.request.urlretrieve(src, fileName)
+        if src[0:4] == 'http':
+            continue
         else:
+            if src[0] == '/':
+                src = src[1:]
+            src = src.replace(' ', '%20')
             mySrc = url + '/' + src
             nameLength = len(src.split('/')[-1])
             newPath = path+'/'+src[0:len(src)-nameLength-1]
             createSubDirectories(newPath)
-            fileName = path+'/'+src
+            fileName = path+'/'+src.replace('%20', ' ')
+            print('Downloading ',fileName)
             urllib.request.urlretrieve(mySrc, fileName)
 
+    divs = content.find_all('div')
+    for div in divs:
+        if div.has_attr('style'):
+            try:
+                div_style = div['style']
+                style = cssutils.parseStyle(div_style)
+                src = style['background-image']
+                mySrc = url + '/' + src
+                nameLength = len(src.split('/')[-1])
+                newPath = path + '/' + src[0:len(src) - nameLength - 1]
+                createSubDirectories(newPath)
+                fileName = path + '/' + src
+                print('Downloading ', mySrc)
+                urllib.request.urlretrieve(mySrc, fileName)
+            except:
+                pass
+
+def findCss(content, path):
+    global url
+    links = content.find_all('link')
+    for link in links:
+        if link['href'][-4:] == '.css' and link['href'][:4]!='http':
+            src = link['href']
+            mySrc = url + '/' + src
+            nameLength = len(src.split('/')[-1])
+            newPath = path + '/' + src[0:len(src) - nameLength - 1]
+            createSubDirectories(newPath)
+            fileName = path + '/' + src
+            file = open(f'{fileName}', 'w+', encoding="utf-8")
+            r = requests.get(mySrc)
+            content = BeautifulSoup(r.content, "html5lib")
+            file.write(str(content)[25:-14])
+        if link['href'][-4:] == '.png' or link['href'][-4:] == '.jpg':
+            src = link['href']
+            mySrc = url + '/' + src
+            nameLength = len(src.split('/')[-1])
+            newPath = path + '/' + src[0:len(src) - nameLength - 1]
+            createSubDirectories(newPath)
+            fileName = path + '/' + src
+            urllib.request.urlretrieve(mySrc, fileName)
+
+def findJs(content, path):
+    global url
+    links = content.find_all('script')
+    for link in links:
+        try:
+            if link['src'][-3:] == '.js' and link['src'][:4]!='http':
+                src = link['src']
+                mySrc = url + '/' + src
+                nameLength = len(src.split('/')[-1])
+                newPath = path + '/' + src[0:len(src) - nameLength - 1]
+                createSubDirectories(newPath)
+                fileName = path + '/' + src
+                file = open(f'{fileName}', 'w+', encoding="utf-8")
+                r = requests.get(mySrc)
+                content = BeautifulSoup(r.content, "html5lib")
+                file.write(str(content)[25:-14])
+        except:
+            continue
 
 createProjectDirectory()
+print("Web site has been scraped successfully.")
 # getIndexHtml()
